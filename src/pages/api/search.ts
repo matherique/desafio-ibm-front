07b451f7180e0d2719dd975bfb8a5246efe8ebf6
Matheus Henrique <matherique@gmail.com>
2021-05-22 19:48:43 -0300

    add pagination into book search

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -3,18 +3,25 @@ import api from 'services/api'
 import { SearchResponse } from 'types'
 import { parseBookInfo } from 'utils/parser'
 
+const TEMPO_CACHE = 60 * 60
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const query = (req.query?.q as string) || ''
+  const { q, startIndex } = req.query as { q: string; startIndex: string }
 
-  if (!query || query.length <= 4) return res.send({ books: [] })
+  if (!q || q.length <= 4) return res.send({ books: [] })
 
-  const { data } = await api.get<SearchResponse>(`?q=${encodeURI(query)}`)
+  const { data } = await api.get<SearchResponse>(
+    `?q=${encodeURI(q)}&startIndex=${startIndex}`
+  )
 
   const bookList = data.items.map(parseBookInfo)
-
+  res.setHeader(
+    'Cache-Control',
+    `s-maxage=${TEMPO_CACHE}, stale-while-revalidate`
+  )
   res.setHeader('Content-Type', 'application/json')
   res.status(200).json({
     total: data.totalItems,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,28 @@ import BookList from 'components/book-list'
 import * as React from 'react'
 import { BookInfo } from '../types'
 
+const PER_PAGE = 10
+const DEFAULT_START_INDEX = 0
+
 function Home(): JSX.Element {
+  const [startIndex, setStartIndex] = React.useState(DEFAULT_START_INDEX)
+  const [totalItemsFound, setTotalBooksFound] = React.useState<number>(0)
   const [query, setQuery] = React.useState('')
   const [books, setBooks] = React.useState<BookInfo[]>([])
   const [error, setError] = React.useState(false)
 
-  const searchUrl = (term: string) => `/api/search?q=${encodeURI(term)}`
+  const searchUrl = (term: string, index: number) =>
+    `/api/search?q=${encodeURI(term)}&startIndex=${index}`
 
   React.useEffect(() => {
     if (!query.length) return
     const timetoutId = setTimeout(() => {
-      fetch(searchUrl(query))
+      fetch(searchUrl(query, startIndex))
         .then(res => res.json())
-        .then(res => setBooks(res.books))
+        .then(res => {
+          setTotalBooksFound(res.total)
+          setBooks(res.books)
+        })
         .catch(() => {
           setBooks([])
           setError(true)
@@ -22,7 +31,17 @@ function Home(): JSX.Element {
     }, 500)
 
     return () => clearTimeout(timetoutId)
-  }, [query])
+  }, [query, startIndex])
+
+  const handleNext = React.useCallback(
+    () => setStartIndex(old => old + PER_PAGE),
+    []
+  )
+
+  const handlePrev = React.useCallback(
+    () => setStartIndex(old => old - PER_PAGE),
+    []
+  )
 
   return (
     <div>
@@ -31,10 +50,23 @@ function Home(): JSX.Element {
         value={query}
         onChange={e => setQuery(e.target.value)}
       />
-      {query ? <p>resultados para: {query}</p> : null}
       {error ? <p style={{ color: '#f00' }}>something went wrong</p> : null}
-
-      <BookList books={books} />
+      {books.length ? (
+        <>
+          <p>{query ? `resultados para: ${query}` : null}</p>
+          <p>foi encontrado: {totalItemsFound} resultados</p>
+          <button disabled={startIndex === 0} onClick={() => handlePrev()}>
+            prev
+          </button>
+          <button
+            disabled={startIndex + PER_PAGE > totalItemsFound}
+            onClick={() => handleNext()}
+          >
+            next
+          </button>
+          <BookList books={books} />
+        </>
+      ) : null}
     </div>
   )
 }
