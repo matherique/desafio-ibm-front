4e9e1d8b2ffb16b6076879c73d3d79ddc6a88db7
Matheus Henrique <matherique@gmail.com>
2021-05-23 10:39:57 -0300

    remove unused code and imports

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,34 +1,13 @@
 import * as React from 'react'
-import { GetStaticProps, GetStaticPaths } from 'next'
 import { NextRouter, withRouter } from 'next/router'
 
 import BookDetails from 'components/book-details'
 import { useFetch } from 'utils/useFetch'
 import { BookInfo } from 'types'
-import { parseBookInfo } from 'utils/parser'
 
 type BookProps = {
   router: NextRouter
 }
-
-/*
-export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    paths: [],
-    fallback: true
-  }
-}
-
-export const getStaticProps: GetStaticProps<HomeProps> = async context => {
-  const id = context.params.id as string
-
-  return {
-    props: {
-      id
-    }
-  }
-}
-  */
 
 function Book({ router }: BookProps): JSX.Element {
   const id = router.query.id as string
@@ -38,11 +17,10 @@ function Book({ router }: BookProps): JSX.Element {
 
   const { data } = useFetch<BookInfo>(bookUrl(id))
 
-  if (!data) return <p>carregando...</p>
+  if (!router.isReady || !data) return <p>carregando...</p>
 
   return (
     <>
-      <h1>Book #{id}</h1>
       <BookDetails info={data} />
       <button onClick={() => router.push('/')}>voltar</button>
     </>
